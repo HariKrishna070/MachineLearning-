c0e8693f4cb4923127a96fcc0449f06e474a8bf1
Hari Krishna <110671039+HariKrishna070@users.noreply.github.com>
2023-05-24 12:13:06 +0530

    Update LinearRegression.py

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -1,16 +1,16 @@
 """
     Linear Regression
 """
+# importing important modules
+import numpy as np 
 
-import numpy as np
-
+# linear regression from scratch
 class LinearRegression:
-    def __init__(self,l_rate=0.01,n_iter = 300):
+    def __init__(self,l_rate=0.01,n_iter = 300):  # initializing hyperameters i.e, learning rate as l_rate and number of iterations should be done as n_iter
         self.l_rate = l_rate
         self.n_iter = n_iter
-        self.slope = None
-        self.intercept = None
         
+    # training model and finding slope and intercept of the linear line which fits the given data
     def fit(self,X_train,y_train):
         n_samples,n_features = X_train.shape
         self.slope = np.zeros(n_features)
@@ -23,9 +23,13 @@ class LinearRegression:
             
             self.slope = self.slope - self.l_rate * w  
             self.intercept = self.intercept - self.l_rate * b
-
+    
+    # pridicting the values of required data
     def predict(self,X_test):
         return [x*self.slope + self.intercept for x in X_test]
     
+    # method to know the values of slope and intercept of the linear line
     def values(self):
         return f"slope : {self.slope}\nintercept : {self.intercept}"
+    
+    
